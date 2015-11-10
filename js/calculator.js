35629613236278ff6dcf3fac1f559fcb15adc273
Jonathan AhNee <jonahnee@gmail.com>
2015-11-10 00:57:01 -1000

    work in progres

---
+++ js/calculator.js
@@ -0,0 +1,28 @@
+var calculator = {
+  total: 0.00,
+
+  add: function (n) {
+    return this.total += n;
+  },
+
+  subtract: function (n) {
+    return this.total -= n;
+  },
+
+  multiply: function (n) {
+    return this.total *= n;
+  },
+
+  divide: function (n) {
+    return this.total /= n;
+  },
+  
+  clear: function () {
+    this.total = 0;
+  },
+
+  equals: function () {
+    return this.total;
+  }
+
+};
